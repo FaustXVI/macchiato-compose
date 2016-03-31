2f637025efc8c35e3388d0e893bbed73e0724f14
souf <khaled.souf@jcdecaux.fr>
2016-03-31 14:55:47 +0200

    creating modules

--- macchiato-compose-core/src/integrationTest/java/com/github/vdemeester/macchiato/compose/CreateContainerIntegrationTest.java
+++ macchiato-compose-core/src/integrationTest/java/com/github/vdemeester/macchiato/compose/CreateContainerIntegrationTest.java
@@ -10,9 +10,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
